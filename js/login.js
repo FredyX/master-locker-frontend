5eefc8c3705655369bcd4d58aba867a307bffa8c
FredyX <feamador@unah.hn>
2025-05-04 17:42:38 -0600

    connected login with the backend

--- js/login.js
+++ js/login.js
@@ -1,30 +1,54 @@
 const form = document.querySelector(".form-container");
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  
-  const { isValidate, message } = await login(false);
-  console.log(isValidate);
-  if (isValidate) {    
+
+  const inputUsername = document.querySelector("#username");
+  const inputPassword = document.querySelector("#user-password");
+
+  const formData = {
+    username: inputUsername.value,
+    password: inputPassword.value,
+  };
+
+  const response = await login(formData);
+
+  if (response?.isValidate) {
+    window.localStorage.setItem("token", response.token);
     window.location.href = "./admin.html";
   } else {
-    messageError(message);
+    messageError(response?.message);
   }
 });
+
 //Funcion que maneja el mensaje de error
 function messageError(message) {
   const divMessageError = document.querySelector(".error-message");
-  console.log(message);
   divMessageError.textContent = message;
   setTimeout(() => {
-    divMessageError.textContent = '';
+    divMessageError.textContent = "";
   }, 4000);
 }
 
-async function login(isValidate) {
-  return isValidate
-    ? { isValidate , message: "Exitoso" }
-    : {
-        isValidate,
-        message: "El usuario o la contraseña es incorrecto",
-      };
+async function login(formData) {
+  try {
+    const data = await fetch("http://localhost:5008/auth/login", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+    const responseError = {
+      isValidate: false,
+      message: "Usuario o Contraseña incorrecta",
+      token: "",
+    };
+
+    if (!data.ok) {
+      return responseError;
+    } else {
+      response = await data.json();
+      return { isValidate: true, message: "Exitoso", token: response.token };
+    }
+  } catch (e) {
+    return responseError;
+  }
 }
